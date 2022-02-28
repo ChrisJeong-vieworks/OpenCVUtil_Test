c1adbab5a04af3f4e3cb7a97ff0c4864072a79cf
Chris Jeong(Vieworks) <2021670@vieworks.com>
2022-02-28 10:02:37 +0900

    first commit !

---
+++ OpenCVUtil_Vieworks/OpenCVUtil_Vieworks/OpenCVUtil_VieworksDlg.h
@@ -0,0 +1,34 @@
+﻿
+// OpenCVUtil_VieworksDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// COpenCVUtilVieworksDlg 대화 상자
+class COpenCVUtilVieworksDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	COpenCVUtilVieworksDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_OPENCVUTIL_VIEWORKS_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
